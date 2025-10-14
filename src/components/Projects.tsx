ee27a602094bd9d66bcdb31df0f280162410ef4e
zrncrblln <zorencorbillon@gmail.com>
2025-10-15 03:37:58 +0800

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
-import { motion } from "motion/react";
-import { useInView } from "motion/react";
+import { motion, useInView } from "motion/react";
 import { useRef, useState } from "react";
 import {
   ExternalLink,
