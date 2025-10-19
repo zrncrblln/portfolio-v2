4656ac029b9d9b3709c9fbd5dbe67f442639b1fd
zrncrblln <zorencorbillon@gmail.com>
2025-10-20 04:36:53 +0800

    Fix BASE_URL modifier error by removing readonly from ImportMetaEnv interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  BASE_URL: string;
+  // add other env variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
